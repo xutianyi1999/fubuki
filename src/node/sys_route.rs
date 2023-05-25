341266953d750bbfa74f7c6e915d51ea5016bf17
xutianyi <853122726@qq.com>
2023-05-25 10:38:11 +0800

    update

--- src/node/sys_route.rs
+++ src/node/sys_route.rs
@@ -1,8 +1,8 @@
 use std::pin::pin;
 
 use anyhow::Result;
-use net_route::{Handle, Route};
 use futures_util::stream::StreamExt;
+use net_route::{Handle, Route};
 
 pub struct SystemRouteHandle {
     handle: Handle,
@@ -33,7 +33,35 @@ impl SystemRouteHandle {
 
     pub async fn add(&mut self, routes: &[Route]) -> Result<()> {
         for x in routes {
+            #[cfg(target_os = "macos")]
+            {
+                use std::process::Stdio;
+                use anyhow::anyhow;
+                use tokio::process::Command;
+
+                let status = Command::new("route")
+                    .args([
+                        "-n",
+                        "add",
+                        "-net",
+                        x.destination.to_string().as_str(),
+                        "-netmask",
+                        x.mask().to_string().as_str(),
+                        x.gateway.unwrap().to_string().as_str(),
+                    ])
+                    .stderr(Stdio::inherit())
+                    .output()
+                    .await?
+                    .status;
+
+                if !status.success() {
+                    return Err(anyhow!("failed to add route"));
+                }
+            }
+
+            #[cfg(not(target_os = "macos"))]
             self.handle.add(x).await?;
+
             self.routes.push(x.clone());
         }
         Ok(())
