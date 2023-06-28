ea365b9934e82d2c7fade143bd0a3019dbc5a7c9
xutianyi <xutianyi1999@live.com>
2023-06-28 13:11:16 +0800

    add windows gui program to request administrator privileges

--- build.rs
+++ build.rs
@@ -10,4 +10,24 @@ fn main() {
             .to_resource_dir()
             .build().unwrap();
     }
+
+    #[cfg(all(target_os = "windows", feature = "gui"))]
+    {
+        let mut res = winres::WindowsResource::new();
+
+        res.set_manifest(r#"
+        <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+        <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
+            <security>
+                <requestedPrivileges>
+                    <requestedExecutionLevel level="requireAdministrator" uiAccess="false" />
+                </requestedPrivileges>
+            </securty>
+        </trustInfo>
+        </assembly>
+        "#);
+
+        res.compile()
+            .unwrap();
+    }
 }
