28a4667d622daa172df808ef5d5a83bdf1f0322a
xutianyi <xutianyi1999@live.com>
2023-11-24 13:56:20 +0800

    update workflow

--- src/main.rs
+++ src/main.rs
@@ -50,7 +50,7 @@ mod nat;
 mod web;
 mod routing_table;
 
-#[cfg(not(target_env = "musl"))]
+#[cfg(feature = "mimalloc")]
 #[global_allocator]
 static GLOBAL: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
