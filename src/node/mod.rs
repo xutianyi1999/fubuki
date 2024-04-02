xutianyi1999/fubuki-test

upgrade hyper to v1.0

src/node/mod.rs
@@ -2,7 +2,7 @@ use std::borrow::Cow;
 use std::cell::SyncUnsafeCell;
 use std::mem::size_of;
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr};
-use std::ops::Range;
+use std::ops::{Deref, Range};
 use std::sync::Arc;
 use std::sync::atomic::{AtomicBool, AtomicU32, AtomicU64, Ordering};
 use std::time::Duration;
@@ -16,8 +16,9 @@ use chrono::Utc;
 use crossbeam_utils::atomic::AtomicCell;
 use futures_util::future::BoxFuture;
 use futures_util::FutureExt;
-use hyper::{Body, Method, Request};
-use hyper::body::Buf;
+use http_body_util::{BodyExt, Empty};
+use hyper::{Method, Request};
+use hyper_util::rt::TokioIo;
 use ipnet::Ipv4Net;
 use linear_map::LinearMap;
 use net_route::Route;
@@ -1631,21 +1632,22 @@ pub(crate) async fn info(api_addr: &str, info_type: NodeInfoType) -> Result<()> 
     let req = Request::builder()
         .method(Method::GET)
         .uri(format!("http://{}/info", api_addr))
-        .body(Body::empty())?;
+        .body(Empty::<hyper::body::Bytes>::new())?;
 
-    let c = hyper::client::Client::new();
-    let resp = c.request(req).await?;
+    let stream = TcpStream::connect(api_addr).await?;
+    let stream = TokioIo::new(stream);
+    let (mut sender, _conn) = hyper::client::conn::http1::handshake(stream).await?;
+    let resp = sender.send_request(req).await?;
 
     let (parts, body) = resp.into_parts();
+    let bytes = body.collect().await?.to_bytes();
 
     if parts.status != 200 {
-        let bytes = hyper::body::to_bytes(body).await?;
         let msg = String::from_utf8(bytes.to_vec())?;
         return Err(anyhow!("http response code: {}, message: {}", parts.status.as_u16(), msg));
     }
 
-    let body = hyper::body::aggregate(body).await?;
-    let interfaces_info: Vec<InterfaceInfo> = serde_json::from_reader(body.reader())?;
+    let interfaces_info: Vec<InterfaceInfo> = serde_json::from_slice(bytes.deref())?;
 
     let mut table = Table::new();
 

src/server/api.rs
@@ -1,10 +1,13 @@
-use std::convert::Infallible;
 use std::net::SocketAddr;
 use std::sync::Arc;
 
 use anyhow::Result;
-use hyper::{Body, http, Request, Response};
-use hyper::service::{make_service_fn, service_fn};
+use http_body_util::Full;
+use hyper::{http, Request, Response};
+use hyper::body::{Bytes, Incoming};
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use tokio::net::TcpListener;
 
 use crate::server::{GroupHandle, GroupInfo};
 
@@ -13,9 +16,9 @@ struct Context {
 }
 
 fn info(
-    _req: Request<Body>,
+    _req: Request<Incoming>,
     group_handles: &[Arc<GroupHandle>],
-) -> Result<Response<Body>, http::Error> {
+) -> Result<Response<Full<Bytes>>, http::Error> {
     let mut list = Vec::with_capacity(group_handles.len());
 
     for gh in group_handles {
@@ -23,66 +26,61 @@ fn info(
     }
 
     let resp = match serde_json::to_vec(&list) {
-        Ok(v) => Response::new(Body::from(v)),
+        Ok(v) => Response::new(Full::new(Bytes::from(v))),
         Err(e) => {
             error!("api server error: {}", e);
 
             Response::builder()
                 .status(500)
-                .body(Body::from(e.to_string()))?
+                .body(Full::new(Bytes::from(e.to_string())))?
         }
     };
     Ok(resp)
 }
 
-fn router(
-    ctx: Arc<Context>,
-    req: Request<Body>,
-) -> Result<Response<Body>, http::Error> {
+fn router(ctx: Arc<Context>, req: Request<Incoming>) -> Result<Response<Full<Bytes>>, http::Error> {
     let path = req.uri().path();
 
     match path {
         "/info" => info(req, ctx.group_handles.as_slice()),
-        "/type" => Ok(Response::new(Body::from("server"))),
+        "/type" => Ok(Response::new(Full::new(Bytes::from("node")))),
         #[cfg(feature = "web")]
         path => crate::web::static_files(path.trim_start_matches('/')),
         #[cfg(not(feature = "web"))]
-        _ => {
-            Response::builder()
-                .status(404)
-                .body(Body::empty())
-        }
+        _ => Response::builder()
+            .status(404)
+            .body(Full::new(Bytes::new())),
     }
 }
 
-pub(super) async fn api_start(
-    bind: SocketAddr,
-    ghs: Vec<Arc<GroupHandle>>
-) -> Result<()> {
-    let ctx = Context {
-        group_handles: ghs,
-    };
+pub(super) async fn api_start(bind: SocketAddr, ghs: Vec<Arc<GroupHandle>>) -> Result<()> {
+    let ctx = Context { group_handles: ghs };
 
     let ctx = Arc::new(ctx);
 
-    let make_svc = make_service_fn(move |_conn|  {
-        let ctx = ctx.clone();
-
-        async move {
-            Ok::<_, Infallible>(service_fn(move |req| {
-                let ctx = ctx.clone();
-
-                async {
-                    router(ctx, req)
-                }
-            }))
-        }
-    });
-
+    let listener = TcpListener::bind(bind).await?;
     info!("api listening on http://{}", bind);
 
-    tokio::spawn(hyper::server::Server::try_bind(&bind)?
-        .serve(make_svc)).await??;
+    loop {
+        let (stream, _) = listener.accept().await?;
+        let stream = hyper_util::rt::TokioIo::new(stream);
+        let ctx = ctx.clone();
 
-    Ok(())
+        tokio::spawn(async move {
+            let res = http1::Builder::new()
+                .serve_connection(
+                    stream,
+                    service_fn(move |req| {
+                        let ctx = ctx.clone();
+
+                        async { router(ctx, req) }
+                    }),
+                )
+                .await;
+
+            if let Err(e) = res {
+                error!("error serving connection: {:?}", e);
+            }
+        });
+    }
 }

src/server/mod.rs
@@ -1,5 +1,6 @@
 use std::mem::size_of;
 use std::net::{Ipv4Addr, SocketAddr};
+use std::ops::Deref;
 use std::sync::Arc;
 use std::time::Duration;
 
@@ -8,8 +9,9 @@ use anyhow::{anyhow, Context, Result};
 use arc_swap::ArcSwap;
 use chrono::Utc;
 use crossbeam_utils::atomic::AtomicCell;
-use hyper::{Body, Method, Request};
-use hyper::body::Buf;
+use http_body_util::{BodyExt, Empty};
+use hyper::{Method, Request};
+use hyper_util::rt::TokioIo;
 use ipnet::Ipv4Net;
 use parking_lot::{Mutex, RwLock};
 use prettytable::{row, Table};
@@ -970,24 +972,24 @@ pub(crate) async fn start<K>(config: ServerConfigFinalize<K>) -> Result<()>
 
 pub(crate) async fn info(api_addr: &str, info_type: ServerInfoType) -> Result<()> {
     let req = Request::builder()
-        .method(Method::GET)
-        .uri(format!("http://{}/info", api_addr))
-        .body(Body::empty())?;
+    .method(Method::GET)
+    .uri(format!("http://{}/info", api_addr))
+    .body(Empty::<hyper::body::Bytes>::new())?;
 
-    let c = hyper::client::Client::new();
-    let resp = c.request(req).await?;
+    let stream = TcpStream::connect(api_addr).await?;
+    let stream = TokioIo::new(stream);
+    let (mut sender, _conn) = hyper::client::conn::http1::handshake(stream).await?;
+    let resp = sender.send_request(req).await?;
 
     let (parts, body) = resp.into_parts();
+    let bytes = body.collect().await?.to_bytes();
 
     if parts.status != 200 {
-        let bytes = hyper::body::to_bytes(body).await?;
         let msg = String::from_utf8(bytes.to_vec())?;
         return Err(anyhow!("http response code: {}, message: {}", parts.status.as_u16(), msg));
     }
 
-    let body = hyper::body::aggregate(body).await?;
-    let groups: Vec<GroupInfo> = serde_json::from_reader(body.reader())?;
-
+    let groups: Vec<GroupInfo> = serde_json::from_slice(bytes.deref())?;
     let mut table = Table::new();
 
     match info_type {

xutianyi1999/fubuki

src/node/mod.rs
@@ -2,7 +2,7 @@ use std::borrow::Cow;
 use std::cell::SyncUnsafeCell;
 use std::mem::size_of;
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr, SocketAddr};
-use std::ops::Range;
+use std::ops::{Deref, Range};
 use std::sync::Arc;
 use std::sync::atomic::{AtomicBool, AtomicU32, AtomicU64, Ordering};
 use std::time::Duration;
@@ -16,8 +16,9 @@ use chrono::Utc;
 use crossbeam_utils::atomic::AtomicCell;
 use futures_util::future::BoxFuture;
 use futures_util::FutureExt;
-use hyper::{Body, Method, Request};
-use hyper::body::Buf;
+use http_body_util::{BodyExt, Empty};
+use hyper::{Method, Request};
+use hyper_util::rt::TokioIo;
 use ipnet::Ipv4Net;
 use linear_map::LinearMap;
 use net_route::Route;
@@ -1631,21 +1632,24 @@ pub(crate) async fn info(api_addr: &str, info_type: NodeInfoType) -> Result<()> 
     let req = Request::builder()
         .method(Method::GET)
         .uri(format!("http://{}/info", api_addr))
-        .body(Body::empty())?;
+        .body(Empty::<hyper::body::Bytes>::new())?;
 
-    let c = hyper::client::Client::new();
-    let resp = c.request(req).await?;
+    let stream = TcpStream::connect(api_addr).await?;
+    let stream = TokioIo::new(stream);
+    let (mut sender, conn) = hyper::client::conn::http1::handshake(stream).await?;
 
+    tokio::spawn(conn);
+
+    let resp = sender.send_request(req).await?;
     let (parts, body) = resp.into_parts();
+    let bytes = body.collect().await?.to_bytes();
 
     if parts.status != 200 {
-        let bytes = hyper::body::to_bytes(body).await?;
         let msg = String::from_utf8(bytes.to_vec())?;
         return Err(anyhow!("http response code: {}, message: {}", parts.status.as_u16(), msg));
     }
 
-    let body = hyper::body::aggregate(body).await?;
-    let interfaces_info: Vec<InterfaceInfo> = serde_json::from_reader(body.reader())?;
+    let interfaces_info: Vec<InterfaceInfo> = serde_json::from_slice(bytes.deref())?;
 
     let mut table = Table::new();
 

src/server/api.rs
@@ -1,10 +1,13 @@
-use std::convert::Infallible;
 use std::net::SocketAddr;
 use std::sync::Arc;
 
 use anyhow::Result;
-use hyper::{Body, http, Request, Response};
-use hyper::service::{make_service_fn, service_fn};
+use http_body_util::Full;
+use hyper::{http, Request, Response};
+use hyper::body::{Bytes, Incoming};
+use hyper::server::conn::http1;
+use hyper::service::service_fn;
+use tokio::net::TcpListener;
 
 use crate::server::{GroupHandle, GroupInfo};
 
@@ -13,9 +16,9 @@ struct Context {
 }
 
 fn info(
-    _req: Request<Body>,
+    _req: Request<Incoming>,
     group_handles: &[Arc<GroupHandle>],
-) -> Result<Response<Body>, http::Error> {
+) -> Result<Response<Full<Bytes>>, http::Error> {
     let mut list = Vec::with_capacity(group_handles.len());
 
     for gh in group_handles {
@@ -23,66 +26,59 @@ fn info(
     }
 
     let resp = match serde_json::to_vec(&list) {
-        Ok(v) => Response::new(Body::from(v)),
+        Ok(v) => Response::new(Full::new(Bytes::from(v))),
         Err(e) => {
             error!("api server error: {}", e);
 
             Response::builder()
                 .status(500)
-                .body(Body::from(e.to_string()))?
+                .body(Full::new(Bytes::from(e.to_string())))?
         }
     };
     Ok(resp)
 }
 
-fn router(
-    ctx: Arc<Context>,
-    req: Request<Body>,
-) -> Result<Response<Body>, http::Error> {
+fn router(ctx: &Context, req: Request<Incoming>) -> Result<Response<Full<Bytes>>, http::Error> {
     let path = req.uri().path();
 
     match path {
         "/info" => info(req, ctx.group_handles.as_slice()),
-        "/type" => Ok(Response::new(Body::from("server"))),
+        "/type" => Ok(Response::new(Full::new(Bytes::from("server")))),
         #[cfg(feature = "web")]
         path => crate::web::static_files(path.trim_start_matches('/')),
         #[cfg(not(feature = "web"))]
-        _ => {
-            Response::builder()
-                .status(404)
-                .body(Body::empty())
-        }
+        _ => Response::builder()
+            .status(404)
+            .body(Full::new(Bytes::new())),
     }
 }
 
-pub(super) async fn api_start(
-    bind: SocketAddr,
-    ghs: Vec<Arc<GroupHandle>>
-) -> Result<()> {
-    let ctx = Context {
-        group_handles: ghs,
-    };
-
+pub(super) async fn api_start(bind: SocketAddr, ghs: Vec<Arc<GroupHandle>>) -> Result<()> {
+    let ctx = Context { group_handles: ghs };
     let ctx = Arc::new(ctx);
 
-    let make_svc = make_service_fn(move |_conn|  {
-        let ctx = ctx.clone();
-
-        async move {
-            Ok::<_, Infallible>(service_fn(move |req| {
-                let ctx = ctx.clone();
-
-                async {
-                    router(ctx, req)
-                }
-            }))
-        }
-    });
-
+    let listener = TcpListener::bind(bind).await?;
     info!("api listening on http://{}", bind);
 
-    tokio::spawn(hyper::server::Server::try_bind(&bind)?
-        .serve(make_svc)).await??;
+    loop {
+        let (stream, _) = listener.accept().await?;
+        let stream = hyper_util::rt::TokioIo::new(stream);
+        let ctx = ctx.clone();
 
-    Ok(())
+        tokio::spawn(async move {
+            let ctx = &ctx;
+            let res = http1::Builder::new()
+                .serve_connection(
+                    stream,
+                    service_fn(move |req| {
+                         std::future::ready(router(ctx, req))
+                    }),
+                )
+                .await;
+
+            if let Err(e) = res {
+                error!("error serving connection: {:?}", e);
+            }
+        });
+    }
 }

src/server/mod.rs
@@ -1,5 +1,6 @@
 use std::mem::size_of;
 use std::net::{Ipv4Addr, SocketAddr};
+use std::ops::Deref;
 use std::sync::Arc;
 use std::time::Duration;
 
@@ -8,8 +9,9 @@ use anyhow::{anyhow, Context, Result};
 use arc_swap::ArcSwap;
 use chrono::Utc;
 use crossbeam_utils::atomic::AtomicCell;
-use hyper::{Body, Method, Request};
-use hyper::body::Buf;
+use http_body_util::{BodyExt, Empty};
+use hyper::{Method, Request};
+use hyper_util::rt::TokioIo;
 use ipnet::Ipv4Net;
 use parking_lot::{Mutex, RwLock};
 use prettytable::{row, Table};
@@ -970,24 +972,26 @@ pub(crate) async fn start<K>(config: ServerConfigFinalize<K>) -> Result<()>
 
 pub(crate) async fn info(api_addr: &str, info_type: ServerInfoType) -> Result<()> {
     let req = Request::builder()
-        .method(Method::GET)
-        .uri(format!("http://{}/info", api_addr))
-        .body(Body::empty())?;
+    .method(Method::GET)
+    .uri(format!("http://{}/info", api_addr))
+    .body(Empty::<hyper::body::Bytes>::new())?;
 
-    let c = hyper::client::Client::new();
-    let resp = c.request(req).await?;
+    let stream = TcpStream::connect(api_addr).await?;
+    let stream = TokioIo::new(stream);
+    let (mut sender, conn) = hyper::client::conn::http1::handshake(stream).await?;
 
+    tokio::spawn(conn);
+
+    let resp = sender.send_request(req).await?;
     let (parts, body) = resp.into_parts();
+    let bytes = body.collect().await?.to_bytes();
 
     if parts.status != 200 {
-        let bytes = hyper::body::to_bytes(body).await?;
         let msg = String::from_utf8(bytes.to_vec())?;
         return Err(anyhow!("http response code: {}, message: {}", parts.status.as_u16(), msg));
     }
 
-    let body = hyper::body::aggregate(body).await?;
-    let groups: Vec<GroupInfo> = serde_json::from_reader(body.reader())?;
-
+    let groups: Vec<GroupInfo> = serde_json::from_slice(bytes.deref())?;
     let mut table = Table::new();
 
     match info_type {