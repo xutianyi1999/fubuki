a3ccbb26c6ccfae4a765e433ad0ca235d776751a
xutianyi <xutianyi1999@live.com>
2024-09-25 12:52:06 +0800

    fix gui font display problem

--- src/main.rs
+++ src/main.rs
@@ -25,31 +25,9 @@ fn main() -> ExitCode {
 #[cfg(any(target_os = "windows", target_os = "linux", target_os = "macos"))]
 #[cfg(feature = "gui")]
 fn main() -> ExitCode {
-    use font_kit::handle::Handle;
-    use std::sync::Arc;
-
     human_panic::setup_panic!();
 
-    let mut settings = klask::Settings::default();
-
-    let sys_source = font_kit::source::SystemSource::new();
-
-    if let Ok(handle) = sys_source
-        .select_best_match(&[font_kit::family_name::FamilyName::Monospace], &font_kit::properties::Properties::new())
-    {
-        let font = match handle {
-            Handle::Memory { bytes, .. } => {
-                let bytes = Arc::into_raw(bytes);
-                Some(std::borrow::Cow::Borrowed(unsafe { (&*bytes).as_slice() }))
-            },
-            Handle::Path { path, .. } => match std::fs::read(path) {
-                Ok(font) => Some(std::borrow::Cow::Owned(font)),
-                _ => None,
-            }
-        };
-
-        settings.custom_font = font;
-    }
+    let settings = klask::Settings::default();
 
     klask::run_derived::<Args, _>(settings, |args| {
         if let Err(e) = launch(args) {
